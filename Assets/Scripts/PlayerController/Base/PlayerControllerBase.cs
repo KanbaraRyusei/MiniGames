cb5c3da99107308ee7a76ed4467692df2298abbc
Ryusei1115 <tekunikaru22@gmail.com>
2022-09-04 11:24:36 +0900

    WIP

--- Assets/Scripts/PlayerController/Base/PlayerControllerBase.cs
+++ Assets/Scripts/PlayerController/Base/PlayerControllerBase.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cysharp.Threading.Tasks;
 
 [RequireComponent(typeof(Rigidbody2D),typeof(Collider2D))]
 public abstract class PlayerControllerBase : MonoBehaviour
@@ -8,6 +9,14 @@ public abstract class PlayerControllerBase : MonoBehaviour
     [SerializeField]
     [Header("スピード")]
     private int _speed;
+
+    [SerializeField]
+    [Header("動ける方向")]
+    private MoveDirection _moveDirection = MoveDirection.Horizontal;
+
+    [SerializeField]
+    [Header("クールタイム")]
+    private int _coolTime;
 
     Rigidbody2D _rb;
 
@@ -29,8 +38,26 @@ public abstract class PlayerControllerBase : MonoBehaviour
     {
         float h = Input.GetAxisRaw("Horizontal") * _speed;
         float v = Input.GetAxisRaw("Vertical") * _speed;
+        switch (_moveDirection)
+        {
+            case MoveDirection.All:
+                break;
+            case MoveDirection.Horizontal:
+                v = 0f;
+                break;
+            case MoveDirection.Vertical:
+                h = 0f;
+                break;
+        }
         _rb.velocity = new Vector2(h, v);
     }
 
     protected abstract void Attack();
+
+    enum MoveDirection
+    {
+        All,
+        Horizontal,
+        Vertical
+    }
 }
