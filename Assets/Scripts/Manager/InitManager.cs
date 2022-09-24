a1cc5e85840c1b9f1e73b28be1009954fdadf9c4
Ryusei1115 <tekunikaru22@gmail.com>
2022-09-24 16:59:34 +0900

    WIP

--- Assets/Scripts/Manager/InitManager.cs
+++ Assets/Scripts/Manager/InitManager.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class InitManager : MonoBehaviour
 {
     [SerializeField]
     TimeManager _timeManager;
 
+    [SerializeField]
+    string _garbageCanPath;
+
+    [SerializeField]
+    string _dirtyPersonPath;
+
+    [SerializeField]
+    Vector3 _garbageCanPosition;
+
+    [SerializeField]
+    Vector3 _dirtyPersonPosition;
+
     private void Start()
     {
+        PhotonNetwork.Instantiate(_garbageCanPath, _garbageCanPosition, Quaternion.identity);
         _timeManager.TimerStart();
     }
 }
