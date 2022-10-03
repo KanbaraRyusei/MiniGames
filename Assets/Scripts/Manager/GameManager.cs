f9175f5bf0d400d5636245e248f231bca9e0c60b
Ryusei1115 <tekunikaru22@gmail.com>
2022-10-03 17:48:09 +0900

    微修正

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -5,11 +5,19 @@ using System;
 
 public static class GameManager
 {
+    public static bool IsStartGame { get; private set; } = false;
+
     public static Action OnGameOver;
+
+    public static void GameStart()
+    {
+        IsStartGame = true;
+    }
 
     public static void GameOver()
     {
         OnGameOver?.Invoke();
+        IsStartGame = false;
         SceneLoder.LoadScene("ResultScene");
     }
 }
